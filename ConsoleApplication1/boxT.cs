c337c896540500026c8100bafa84ac8cb32e967e
svvota <cagriersan@gmail.com>
2016-02-07 04:08:06 +0200

    u

--- ConsoleApplication1/boxT.cs
+++ ConsoleApplication1/boxT.cs
@@ -28,16 +28,7 @@ namespace Boxtester
             //length = 6.0;
             //breadth = 7.0;
 
-            //// box 2 specification
-            //height = 10.0;
-            //length = 12.0;
-            //breadth = 13.0;
 
-            // volume of box 1
-            //volume = height * length * breadth;
-            //Console.WriteLine("Kutunun hacmi : {0}", volume);
-
-            // volume of box 2
             volume = height * length * breadth;
             Console.WriteLine("");
             Console.WriteLine("Kutunun hacmi : {0}", volume);
